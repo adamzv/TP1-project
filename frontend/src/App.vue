a7cf313960bc74880fdbcba9152b3686a325ebd5
Adam Zverka <adamzverka@hotmail.com>
2020-10-17 15:33:25 +0200

    Testing out navbar and carousel combination with Buefy
    TODO: find proper logo of the university

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,80 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar class="is-link" :fixed-top="false" :transparent="true">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="../src/assets/logoukf.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item href="#">
+          Home
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Documentation
+        </b-navbar-item>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-success">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+    <b-carousel
+      v-model="carousel"
+      :animated="animated"
+      :has-drag="drag"
+      :autoplay="autoPlay"
+      :pause-hover="pauseHover"
+      :pause-info="pauseInfo"
+      :pause-info-type="pauseType"
+      :interval="interval"
+      :repeat="repeat"
+      @change="info($event)"
+    >
+      <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+        <section :class="`hero is-medium is-${carousel.color} is-bold`">
+          <div class="hero-body has-text-centered">
+            <h1 class="title">{{ carousel.title }}</h1>
+            <h2>{{ carousel.time }}</h2>
+          </div>
+        </section>
+      </b-carousel-item>
+    </b-carousel>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      carousel: 0,
+      pauseInfo: false,
+      interval: 3000,
+      carousels: [
+        { title: "Lorem Ipsum Event 1", color: "dark" },
+        { title: "Lorem Ipsum Event 2", color: "primary" },
+        { title: "Lorem Ipsum Event 3", color: "info" },
+        { title: "Lorem Ipsum Event 4", color: "success" },
+        { title: "Lorem Ipsum Event 5", color: "warning" },
+        { title: "Lorem Ipsum Event 6", color: "danger" }
+      ]
+    };
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -25,5 +93,18 @@
       color: #42b983;
     }
   }
+  // position: absolute;
+  // background: rgb(0, 0, 0);
+  // opacity: 0.85;
+}
+nav.navbar.is-fixed-top {
+  background: transparent;
+}
+nav.navbar.is-link {
+  background: rgba(0, 0, 0, 0);
+  position: absolute;
+}
+body {
+  padding-top: 0px !important;
 }
 </style>
