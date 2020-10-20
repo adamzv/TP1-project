bcaacc846d1f6ef648f95295b5932d53dbf6ec14
Adam Zverka <31664842+adamzv@users.noreply.github.com>
2020-10-20 16:47:13 +0200

    Feature/frontend preparation (#3)
    * Installed and tested Buefy and Material Design Icons
    
    * Added lib for sharing funcionalities and updated prettier
    
    by default prettier formats code like this (to prevent white spaces):
    <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
    >
            vue-cli documentation
    
    to prevent that I added configuration settings to package.json - if in the future there will be problems with whitespaces, this configuration will be reverted
    
    * Testing out navbar and carousel combination with Buefy
    
    TODO: find proper logo of the university
    
    * Added axios and fixed navbar with carousel
    
    also added one test picture from unsplash.com just to test transparent gradient over background image
    
    * Added plugin for i18n, some translations and simple gradients for faculties
    
    * Generated code cleanup
    
    * Update App.vue
    
    * Fixed build error

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,18 @@
+import "@mdi/font/css/materialdesignicons.css";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  i18n,
+  render: (h) => h(App)
 }).$mount("#app");
