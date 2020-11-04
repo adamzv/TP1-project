5040b04bfd8cc85718efaf6655f56bc3a2c91dab
Adam Zverka <adamzverka@hotmail.com>
2020-11-04 15:36:20 +0100

    App.vue cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <HeaderComponent />
-    <p>{{ msg }}</p>
     <FilterComponent />
     <router-view />
     <FooterComponent />
@@ -12,33 +11,12 @@
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
 import FilterComponent from "@/components/FilterComponent.vue";
-import httpClient from "./httpClient";
 
 export default {
   components: {
     HeaderComponent,
     FooterComponent,
     FilterComponent
-  },
-  data() {
-    return {
-      msg: "todo"
-    };
-  },
-  methods: {
-    call() {
-      httpClient
-        .get("/places")
-        .then(response => {
-          this.msg = response;
-        })
-        .catch(error => {
-          this.msg = error;
-        });
-    }
-  },
-  created() {
-    this.call();
   }
 };
 </script>
