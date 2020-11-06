343419131ad0c7a8562b838bbee04e097b859481
Adam Zverka <adamzverka@hotmail.com>
2020-11-06 23:06:41 +0100

    [TP-50] Working on event form

--- frontend/src/views/ManageEvent.vue
+++ frontend/src/views/ManageEvent.vue
@@ -1,44 +1,120 @@
 <template>
   <section>
-    <b-field label="Name">
-      <b-input v-model="name"></b-input>
-    </b-field>
+    <div class="container">
+      <h1 class="is-uppercase is-size-4">Vytvorenie novej udalosti</h1>
+      <hr class="hr" />
 
-    <b-field label="Email" type="is-danger" message="This email is invalid">
-      <b-input type="email" value="john@" maxlength="30"></b-input>
-    </b-field>
-
-    <b-field
-      label="Username"
-      type="is-success"
-      message="This username is available"
-    >
-      <b-input value="johnsilver" maxlength="30"></b-input>
-    </b-field>
-
-    <b-field label="Password">
-      <b-input
-        type="password"
-        value="iwantmytreasure"
-        password-reveal
-      ></b-input>
-    </b-field>
-
-    <b-field label="Message">
-      <b-input maxlength="200" type="textarea"></b-input>
-    </b-field>
+      <div class="box">
+        <div class="columns">
+          <div class="column is-two-fifths">
+            <b-field label="Názov udalosti">
+              <b-input v-model="name"></b-input>
+            </b-field>
+          </div>
+          <div class="column">
+            <b-field label="Kategórie">
+              <b-taginput
+                v-model="categories"
+                :data="filteredCategories"
+                field="name"
+                autocomplete
+                :allow-new="false"
+                :open-on-focus="false"
+                icon="label"
+                placeholder="Pridať kategóriu"
+                @typing="getFilteredTags"
+              ></b-taginput>
+            </b-field>
+            <a @click="isCategoryModalActive = true">Pridať novú kategóriu</a>
+            <b-modal
+              v-model="isCategoryModalActive"
+              has-modal-card
+              trap-focus
+              :destroy-on-hide="false"
+              aria-role="dialog"
+              aria-modal
+            >
+              <template #default="props">
+                <div class="card">
+                  <div class="card-content">
+                    <div class="content">
+                      <b-field label="Name">
+                        <b-input v-model="newCategoryName"></b-input>
+                      </b-field>
+                      <button
+                        class="button is-primary is-medium"
+                        @click="addNewCategory"
+                      >
+                        Launch image modal
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </b-modal>
+          </div>
+          <div class="column"></div>
+          <div class="column"></div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <b-field label="Popis udalosti">
+              <b-input maxlength="500" v-model="desc" type="textarea"></b-input>
+            </b-field>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import httpClient from "../httpClient.js";
+
 export default {
   name: "manageEvent",
+  created() {
+    // loads categories from backend
+    httpClient.get("/categories").then(response => {
+      this.availableCategories = response.data;
+    });
+  },
   data() {
     return {
-      name: "zebrak"
+      newCategoryName: "",
+      isCategoryModalActive: false,
+      availableCategories: [],
+      filteredCategories: [],
+      id: null,
+      name: "Kariérne dni",
+      desc: null,
+      categories: []
     };
+  },
+  methods: {
+    getFilteredTags(text) {
+      this.filteredCategories = this.availableCategories.filter(option => {
+        return (
+          option.name
+            .toString()
+            .toLowerCase()
+            .indexOf(text.toLowerCase()) >= 0
+        );
+      });
+    },
+    addNewCategory() {
+      this.availableCategories.push({ name: this.newCategoryName });
+      this.newCategoryName = "";
+      // TODO add validation
+      // TODO make modal prettier
+      this.isCategoryModalActive = false;
+    }
   }
 };
 </script>
 
-<style></style>
+<style>
+.columns.notification {
+  padding-top: 12px;
+}
+</style>
