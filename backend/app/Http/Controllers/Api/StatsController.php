38cfe068374b0af65dfd2bfdbf02f27c7eac947e
Ľudovít Laca <laca.ludo97@gmail.com>
2020-12-10 21:37:18 +0100

    [TP-118] Added new stats form admin
    * added method for new events count for faculties
    * added counter  for registered users
    * added counter for new events

--- backend/app/Http/Controllers/Api/StatsController.php
+++ backend/app/Http/Controllers/Api/StatsController.php
@@ -14,41 +14,88 @@ use Illuminate\Support\Facades\DB;
 class StatsController extends Controller
 {
     /**
-     * Get number of events for each faculty
+     * Get events according faculties for last year and new
      *
      * @return false|string
      */
-    public function getFacultiesEventsCount()
+    public function getEventsFaculties()
+    {
+        $data = [];
+        $data = $this->getFacultiesLastYearEventsCount($data);
+        $data += $this->getFacultiesNewEventsCount($data);
+
+        return json_encode($data);
+    }
+
+    /**
+     * Get number of events for each faculty for last year
+     *
+     * @param $data
+     * @return false|string
+     */
+    public function getFacultiesLastYearEventsCount($data)
     {
         $query = DB::table('events')
-            ->select(DB::raw('COUNT(events.id) as pocet'), 'faculties.name as meno', 'events.id_faculty as fid', 'events.*')
+            ->select(DB::raw('COUNT(events.id) as pocet'), 'faculties.name as meno')
             ->join('faculties', 'events.id_faculty', '=', 'faculties.id')
             ->whereBetween('events.beginning', [Carbon::now()->subYear(), Carbon::now()])
             ->groupBy('faculties.name')
             ->get();
 
-        $data = [];
-        $array = [];
-        $array2 = [];
-        $array3 = [];
+        $array = ['Fakulty'];
+        $array2 = ['Počet eventov za posledný rok'];
 
         $i = 0;
         foreach ($query as $event) {
-            array_push($array, $event->meno);
-            array_push($array2, $event->pocet);
-
-
-            $array3[$event->meno] = $this->getDepartmentsCount($event->fid, $event->id_department);
+            $array['faculty' . $i] = $event->meno;
+            $array2['pocet' . $i] = $event->pocet;
             $i++;
         }
 
-        $data['faculty'] = $array;
-        $data['count'] = $array2;
-        $data['departments'] = $array3;
+        $data['faculty'] = $json_object = array_values($array);
+        $data['pocet'] = $json_object2 = array_values($array2);
 
-        return json_encode($data);
+        return $data;
     }
 
+    /**
+     * Get number of new events for each faculty
+     *
+     * @param $data
+     * @return false|string
+     */
+    public function getFacultiesNewEventsCount($data)
+    {
+        $query = DB::table('events')
+            ->select(DB::raw('COUNT(events.id) as pocet'), 'faculties.name as meno')
+            ->join('faculties', 'events.id_faculty', '=', 'faculties.id')
+            ->where('events.beginning', '>=', Carbon::now())
+            ->groupBy('faculties.name')
+            ->get();
+
+        $array = ['Fakulty'];
+        $array2 = ['Počet nadchádzajúcich eventov'];
+
+        $i = 0;
+        foreach ($query as $event) {
+            $array['faculty' . $i] = $event->meno;
+            $array2['pocet' . $i] = $event->pocet;
+            $i++;
+        }
+
+        $data['faculty1'] = $json_object = array_values($array);
+        $data['pocet1'] = $json_object2 = array_values($array2);
+
+        return $data;
+    }
+
+    /**
+     * Returns number of events according faculty department (not using)
+     *
+     * @param $id
+     * @param $id_departmentt
+     * @return array
+     */
     public function getDepartmentsCount($id, $id_departmentt)
     {
         $query = DB::table('events')
@@ -75,6 +122,21 @@ class StatsController extends Controller
     }
 
     /**
+     * Returns all stats for admin header
+     *
+     * @return array
+     */
+    public function getStats()
+    {
+        $data = [];
+        array_push($data, $this->getOnlineUsers());
+        array_push($data, $this->getRegisteredUsers());
+        array_push($data, $this->getNewEvents());
+
+        return $data;
+    }
+
+    /**
      * Get number of online users
      *
      * @return Collection
@@ -84,6 +146,35 @@ class StatsController extends Controller
         return $query = DB::table('oauth_access_tokens')
             ->select(DB::raw('COUNT(DISTINCT(user_id)) AS pocet'))
             ->where('oauth_access_tokens.expires_at', '>=', Carbon::now())
+            ->get();
+    }
+
+    /**
+     * Get number of registered users
+     *
+     * @return Collection
+     */
+    public function getRegisteredUsers()
+    {
+        return $query = DB::table('users')
+            ->select(DB::raw('COUNT(DISTINCT(users.id)) as registrovani'))
+            ->join('roles', 'roles.id', '=', 'users.id_role')
+            ->whereNotNull('users.email_verified_at')
+            ->where('roles.type', '!=', 'admin')
+            ->where('roles.type', '!=', 'host<')
+            ->get();
+    }
+
+    /**
+     * Get number of events, that are planned
+     *
+     * @return Collection
+     */
+    public function getNewEvents()
+    {
+        return $query = DB::table('events')
+            ->select(DB::raw('COUNT(DISTINCT(events.id)) as eventy'))
+            ->where('events.beginning', '>=', Carbon::now())
             ->get();
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -62,8 +62,8 @@ Route::namespace('Api')->group(function () {
     Route::get('admin', 'EventsController@admin');
 
     Route::prefix('stats')->group(function () {
-        Route::get('faculties', 'StatsController@getFacultiesEventsCount');
-        Route::get('onlineUsers', 'StatsController@getOnlineUsers');
+        Route::get('faculties', 'StatsController@getEventsFaculties');
+        Route::get('onlineUsers', 'StatsController@getStats');
     });
 
     Route::prefix('files')->group(function () {
